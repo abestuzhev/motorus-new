a74da91d5045d3a374550b645fb561a1c93e77d8
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-03-22 15:17:03 +0300

    changes

--- js/configurator.js
+++ js/configurator.js
@@ -1,16 +1,40 @@
 $(function(){
+
+    var cardConf = '',
+        cardConfWidth = '',
+        windowWidth = $(window).width();
     // var confCategoryItem = $('.conf-category__item');
     $(document).on('click', '.conf-list__link', function(e){
         e.preventDefault();
         $this = $(this);
         $this.toggleClass('show-submenu');
         $this.siblings('.conf-list').slideToggle(100);
+        // $this.siblings('.conf-list').children('.conf-list__item').find('.conf-form').slideToggle(100);
+        cardConf = $('.c-card-configurator'),
+        cardConfWidth = cardConf.width();
     });
+
+
+    $(document).on('click', '.configurator-total__price', function(e){
+        e.preventDefault();
+        $this = $(this);
+        $this.siblings('.configurator-total__body').toggleClass('show-conf-total');
+    });
+
+
+
 
     $(document).on('click', '.conf-list__title', function(e){
         e.preventDefault();
         $this = $(this);
         $this.siblings('.conf-list').slideToggle(100);
+    });
+
+    var cardConf = $('.c-card-configurator'),
+        cardConfWidth = cardConf.width();
+
+    $(window).resize(function(){
+        console.log('Ширина карточки' + cardConfWidth);
     });
 
 
